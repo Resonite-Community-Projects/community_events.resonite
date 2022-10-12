2da901bdf55e043f22510c56eea220dc37c623ca
brodokk <brodokk@brodokk.space>
2022-10-12 18:45:13 +0200

    Remove apollo bot support for now

--- bots/__init__.py
+++ bots/__init__.py
@@ -1,5 +1,4 @@
 from .discord import DiscordScheduledEvents
 from .google import GoogleCalendar
-from .apollo import Apollo
 
-__all__ = [DiscordScheduledEvents, GoogleCalendar, Apollo]
+__all__ = [DiscordScheduledEvents, GoogleCalendar]
