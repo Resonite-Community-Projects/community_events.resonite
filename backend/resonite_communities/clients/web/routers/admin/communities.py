d6db681ce3c712ce8fc74b066aeddf765cb47ca6
brodokk <brodokk@brodokk.space>
2025-03-10 23:32:09 +0000

    List communities in the admin section

--- backend/resonite_communities/clients/web/routers/admin/communities.py
+++ backend/resonite_communities/clients/web/routers/admin/communities.py
@@ -5,7 +5,7 @@ from starlette.responses import RedirectResponse
 
 from resonite_communities.clients.web.utils.templates import templates
 from resonite_communities.clients.web.routers.utils import UserAuthModel, get_user_auth, logo_base64
-
+from resonite_communities.models.community import Community, CommunityPlatform
 router = APIRouter()
 
 @router.get("/admin/communities")
@@ -14,8 +14,14 @@ async def get_communities(request: Request, user_auth: UserAuthModel = Depends(g
     if not user_auth or not user_auth.is_superuser:
         return RedirectResponse(url="/")
 
+    events_communities = Community().find(platform__in=[CommunityPlatform.DISCORD, CommunityPlatform.JSON])
+    streams_communities = Community().find(platform__in=[CommunityPlatform.TWITCH])
+
     return templates.TemplateResponse("admin/communities.html", {
         "userlogo" : logo_base64,
         "user" : deepcopy(user_auth),
+        "events_communities": events_communities,
+        "streams_communities": streams_communities,
         "request": request,
     })
+
