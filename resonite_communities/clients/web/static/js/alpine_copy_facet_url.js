ef426a7f55e31298bd25d2ed1636670f7b564caf
brodokk <brodokk@brodokk.space>
2025-06-05 15:38:26 +0000

    switch copy facet button to alpinejs

---
+++ resonite_communities/clients/web/static/js/alpine_copy_facet_url.js
@@ -0,0 +1,53 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('copyFacetUrl', () => ({
+        init() {
+            // Show the button if JavaScript is enabled
+            document.querySelector('.js-copy-facet-url').style.display = 'block';
+            document.querySelector('.nojs-copy-facet-url').style.display = 'none';
+        },
+        copyToClipboard(text) {
+            var textArea = document.createElement("textarea");
+
+            textArea.style.position = 'fixed';
+            textArea.style.top = 0;
+            textArea.style.left = 0;
+            textArea.style.width = '2em';
+            textArea.style.height = '2em';
+            textArea.style.padding = 0;
+            textArea.style.border = 'none';
+            textArea.style.outline = 'none';
+            textArea.style.boxShadow = 'none';
+            textArea.style.background = 'transparent';
+
+            textArea.value = text;
+
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+
+            const CopyFacetResoniteDBUrl = document.querySelector('.js-copy-facet-url');
+
+            function resetCopyBtn(oldStatus) {
+                CopyFacetResoniteDBUrl.classList.remove(oldStatus);
+                CopyFacetResoniteDBUrl.classList.add('is-dark');
+                CopyFacetResoniteDBUrl.querySelector('span').innerHTML = 'Copy facet resonitedb url';
+            }
+
+            function setStatusCopyBtn(msg, status) {
+                CopyFacetResoniteDBUrl.querySelector('span').innerHTML = msg;
+                CopyFacetResoniteDBUrl.classList.remove('is-dark');
+                CopyFacetResoniteDBUrl.classList.add(status);
+                setTimeout(function () { resetCopyBtn(status); }, 3000);
+            }
+
+            try {
+                var successful = document.execCommand('copy');
+                setStatusCopyBtn('Successfully copied resonitedb url', 'is-success');
+            } catch (err) {
+                setStatusCopyBtn('Error copy resonitedb url', 'is-danger');
+            }
+
+            document.body.removeChild(textArea);
+        }
+    }));
+});
