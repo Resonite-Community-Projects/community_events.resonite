feed37429cb711224854716529d10276a7449f94
brodokk <brodokk@brodokk.space>
2025-09-24 22:49:48 +0000

    Fix login bug

--- resonite_communities/clients/utils/auth.py
+++ resonite_communities/clients/utils/auth.py
@@ -9,8 +9,15 @@ from resonite_communities.auth.users import optional_current_active_user, User
 from resonite_communities.utils.db import get_current_async_session
 from resonite_communities.auth.db import DiscordAccount
 
+class DiscordAccountData(BaseModel):
+    id: int
+    name: str
+    avatar_url: str
+    user_communities: list[str]
+    discord_update_retry_after: int
+
 class UserAuthModel(BaseModel):
-    discord_account: DiscordAccount | None = None
+    discord_account: DiscordAccountData | None = None
     is_superuser: bool | None = False
     is_moderator: bool | None = False
 
@@ -26,6 +33,14 @@ async def get_user_auth(user: User = Depends(optional_current_active_user)) -> O
     session = await get_current_async_session()
     for user_oauth_account in user.oauth_accounts:
         if user_oauth_account.oauth_name == "discord":
-            user_auth_model.discord_account = await session.get(DiscordAccount, user_oauth_account.discord_account_id)
+            discord_account_db = await session.get(DiscordAccount, user_oauth_account.discord_account_id)
+            if discord_account_db:
+                user_auth_model.discord_account = DiscordAccountData(
+                    id=discord_account_db.id,
+                    name=discord_account_db.name,
+                    avatar_url=discord_account_db.avatar_url,
+                    user_communities=discord_account_db.user_communities,
+                    discord_update_retry_after=discord_account_db.discord_update_retry_after
+                )
 
     return user_auth_model
