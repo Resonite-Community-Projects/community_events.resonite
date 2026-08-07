6d9363571eb94bc2492afeefda77210c99a7790c
brodokk <brodokk@brodokk.space>
2026-08-07 09:15:25 +0000

    Clean

--- resonite_communities/api/routes/v2/items.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/items/")
-async def read_items():
-    return [{"name": "Item 1"}, {"name": "Item 2"}]
--- resonite_communities/api/routes/v2/users.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from fastapi_versionizer.versionizer import api_version
-
-router = APIRouter()
-
-@api_version(2, 1)
-@router.get("/users/")
-async def read_users():
-    return [{"name": "Item 1"}, {"name": "Item 2"}]
